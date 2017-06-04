2482e40d2b24c9f55ec9a8aafb7ff4274c126859
METALZEROONE\ibrahim <ibrahimmohammed1002@gmail.com>
2017-06-04 02:44:20 +0200

    input outputs

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MapService } from '../map.service';
+import {
+  Component,
+  OnInit,
+  Input
+} from '@angular/core';
+import {
+  MapService
+} from '../map.service';
 
 declare var mapboxgl;
 @Component({
@@ -8,91 +14,98 @@ declare var mapboxgl;
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
- map:any;
- features:any[];
+  map: any;
+  features: any[];
 
-constructor(private mapservice: MapService) {
-
-}
-
-  ngOnInit() {
-      let map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v10',
-            zoom: 5,
-            center: [29.9900,31.2408],
-            hash:true
-          });
-          map.addControl(new mapboxgl.NavigationControl());
-
-          this.mapservice.map = map;
-          this.map = this.mapservice.map;
-
-          map.on('style.load', function() {
-               map.addSource('census', {
-                    'type': 'vector',
-                    'url': 'mapbox://ibrahimmohammed.8keg391s'
-                });
-              map.addLayer({
-                          "id": "census-data",
-                          "type": "circle",
-                          "source":"census",
-                          "source-layer": "census-bjfccr",
-                          "layout": {
-                              "visibility": "visible"
-                          },
-                          "paint": {
-                              "circle-color": "hsl(247, 54%, 42%)",
-                              "circle-stroke-color": "hsl(0, 0%, 100%)",
-                              "circle-radius": 4,
-                              "circle-stroke-width": 1.5
-                          }
-                      })
-
-          });
-
-          map.on('click', 'census-data', function (e) {
-              var features = e.features[0].properties.Outlet_Nam;
-              console.log()
-              new mapboxgl.Popup()
-           .setLngLat(e.features[0].geometry.coordinates)
-           .setHTML(e.features[0].properties.Outlet_Nam)
-           .addTo(map);
-           });
-
-           map.on('load', () => {
-               this.features = this.map.queryRenderedFeatures({ layers: ['census-data'] });
-               console.log(this.features)
-           })
-
-           // Change the cursor to a pointer when the mouse is over the places layer.
-           map.on('mouseenter', 'census-data', function () {
-               map.getCanvas().style.cursor = 'pointer';
-           });
-
-           // Change it back to a pointer when it leaves.
-           map.on('mouseleave', 'census-data', function () {
-               map.getCanvas().style.cursor = '';
-           });
+  constructor(private mapservice: MapService) {
 
   }
 
-
-      state = !this.state;
-      onChange(event){
-          console.log(event)
-          if(!event){
-              this.map.setLayoutProperty('census-data', 'visibility', 'none');
-          }else {
-              this.map.setLayoutProperty('census-data', 'visibility', 'visible');
-          }
-      }
+  ngOnInit() {
+    let map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [31.2408, 29.9900],
+      zoom: 13,
+      hash: true
+    });
 
 
-    changeStreets(){
-        this.map.setStyle('mapbox://styles/mapbox/streets-v10')
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+    this.mapservice.map = map;
+    this.map = this.mapservice.map;
+
+    map.on('style.load', () => {
+      map.addSource('census', {
+        'type': 'vector',
+        'url': 'mapbox://ibrahimmohammed.8keg391s'
+      });
+
+      map.addLayer({
+        "id": "census-data",
+        "type": "circle",
+        "source": "census",
+        "source-layer": "census-bjfccr",
+        "layout": {
+          "visibility": "visible"
+        },
+        "paint": {
+          "circle-color": "hsl(247, 54%, 42%)",
+          "circle-stroke-color": "hsl(0, 0%, 100%)",
+          "circle-radius": 4,
+          "circle-stroke-width": 1.5
+        }
+      })
+
+    })
+
+
+
+    map.on('load', () => {
+      this.features = this.map.queryRenderedFeatures({
+        layers: ['census-data']
+      });
+      console.log(this.features)
+    })
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'census-data', function () {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'census-data', function () {
+      map.getCanvas().style.cursor = '';
+    });
+
+    map.on('click', 'census-data', function (e) {
+      var features = e.features[0].properties.Outlet_Nam;
+      console.log()
+      new mapboxgl.Popup()
+        .setLngLat(e.features[0].geometry.coordinates)
+        .setHTML(e.features[0].properties.Outlet_Nam)
+        .addTo(map);
+    });
+  }
+
+
+  state = !this.state;
+  onChange(event) {
+    console.log(event)
+    if (!event) {
+      this.map.setLayoutProperty('census-data', 'visibility', 'none');
+    } else {
+      this.map.setLayoutProperty('census-data', 'visibility', 'visible');
     }
-    changeBasic(){
-        this.map.setStyle('mapbox://styles/mapbox/basic-v9')
-    }
+  }
+
+
+  changeStreets() {
+    this.map.setStyle('mapbox://styles/mapbox/streets-v10')
+  }
+  changeBasic() {
+    this.map.setStyle('mapbox://styles/mapbox/basic-v9')
+  }
 }
--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 })
 export class PanelComponent implements OnInit {
 
+  @Input() f:any[];
+
   constructor() { }
 
   ngOnInit() {
